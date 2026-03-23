0e9f82dced8ec915c3dccb156715f81632a5740a
FARHAN <farhan@FARHANs-MacBook-Air.local>
2026-03-23 14:53:46 +0530

    feat: folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Button>Hello</Button>
+    </div>
   )
 }
